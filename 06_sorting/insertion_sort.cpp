c390f0d1aa85a359c0ca55f186bf0b331c5484d7
Dhruv <dhruvdarad04@gmail.com>
2026-08-19 08:18:08 +0530

    Added insertion sort code

--- 06_sorting/insertion_sort.cpp
+++ 06_sorting/insertion_sort.cpp
@@ -57,7 +57,18 @@ Done
 #include <bits/stdc++.h>
 using namespace std;
 
-
+void insertionSort(int arr[], int n){
+    for(int i=1; i<=n-1; i++){
+        for(int j=i; j>0; j--){
+            if(arr[j] < arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            }else{
+                break;
+            }
+        }
+    }
+    
+}
 
 int main(){
     int n;
